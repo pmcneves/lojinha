a0fd4e54d2ca4fc70e5e40aeffe6c8ae00115342
Pedro Neves <pneves355@gmail.com>
2021-09-24 17:02:00 +0100

    header2

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import { Provider } from "react-redux";
-import Header from "./containers/Header";
+import Header from "../src/containers/Header";
 // import AppRouter from './routes';
 // import { fetchDataFromApi } from "./screens/Homepage/actions";
 // import configureStore from "./store/configureStore";
